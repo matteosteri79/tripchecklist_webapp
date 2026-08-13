f70f346a1cf922a7f9f5930e8887435be92ee28c
Matteo Steri <matteo.steri@gmail.com>
2026-08-13 07:39:58 +0200

    Fix variabili utils

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ if ("serviceWorker" in navigator) {
 
 // ---------- Import ----------
 import { openModal, closeModal } from "./modal.js"
-import { APP_VERSION,SUPPORT_EMAIL,PLAY_STORE_URL } from "./utils.js"
+import { APP_NAME, APP_VERSION, SUPPORT_EMAIL, PLAY_STORE_URL } from "./utils.js"
 import {
     initLanguage,
     setLanguage,
@@ -129,7 +129,7 @@ function renderHome(){
 
         welcome.style.display = "block"
         welcome.innerHTML = `
-            <img src="${getLogoImage()}" alt="Trip Checklist">
+            <img src="${getLogoImage()}" alt="${APP_NAME}">
             <p class="logo-tagline">${t("home.tagline")}</p>
             <p>${t("home.welcome")}</p>
             <p>${t("home.createFirst")}</p>
@@ -345,7 +345,7 @@ function addFirstItemModal(sIndex, cIndex){
 function openInfoModal(){
     openModal(`
         <div class="textcenter">
-            <img src="${getLogoImage()}" class="info-logo" alt="Trip Checklist">
+            <img src="${getLogoImage()}" class="info-logo" alt="${APP_NAME}">
             <p class="logo-tagline">${t("info.tagline")}</p>
             <p><strong>v. ${APP_VERSION}</strong></p>
             <p><small>${t("info.description")}<br>
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,3 +1,5 @@
-export const APP_VERSION = "1.7.1"
+
+export const APP_NAME = "Trip Checklist";
+export const APP_VERSION = "1.7.2"
 export const SUPPORT_EMAIL = "matteosteri+tripchecklist@gmail.com";
-export const PLAY_STORE_URL = "";
+export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.matteosteri.tripchecklist";
