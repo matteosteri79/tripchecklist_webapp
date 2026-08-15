8202a4a12610aaf3b2a71bf8c3ae3186302853be
Matteo Steri <matteo.steri@gmail.com>
2026-08-15 17:45:51 +0200

    v1.7.3: Modifica loghi

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,5 +1,5 @@
 
 export const APP_NAME = "Trip Checklist";
-export const APP_VERSION = "1.7.2"
+export const APP_VERSION = "1.7.3"
 export const SUPPORT_EMAIL = "matteosteri+tripchecklist@gmail.com";
 export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.matteosteri.tripchecklist";
